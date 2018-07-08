063a9918a59af912f55f66af24224d019357c086
Anthony LaTorre <tlatorre@uchicago.edu>
2018-07-08 16:01:05 -0400

    second attempt to try and log Flask exceptions
    This time we add gunicorn's error handler to the Flask app's logger, so any
    exceptions should be logged to the gunicorn error logfile.

--- minard/__init__.py
+++ minard/__init__.py
@@ -37,14 +37,22 @@ app = Flask(__name__)
 
 app.config.from_envvar('MINARD_SETTINGS', silent=False)
 
-if not app.debug and 'LOGFILE' in app.config:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler(app.config['LOGFILE'], maxBytes=1e6, backupCount=100)
-    file_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
+# try to configure logging. It is extremely difficult to figure out what the
+# proper way to log exceptions from Flask when running under gunicorn is. I
+# initially tried the solution here:
+# https://stackoverflow.com/questions/14037975/how-do-i-write-flasks-excellent-debug-log-message-to-a-file-in-production
+# however I kept getting permission denied errors when trying to open the file
+# even though I had set up the correct permissions on /var/log/minard.
+# Eventually I started looking for another way to do it and found this post:
+# https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f
+# which discusses how to pipe Flask's messages to the gunicorn logger.
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        import logging
+        gunicorn_logger = logging.getLogger('gunicorn.error')
+        for handler in gunicorn_logger.handlers:
+            app.logger.addHandler(handler)
 
 app.wsgi_app = ReverseProxied(app.wsgi_app)
 
