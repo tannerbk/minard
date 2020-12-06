9a5867dd3ce24981f38f2d69631ea3b6859f7e1e
tannerbk <tannerk@sas.upenn.edu>
2020-12-05 20:29:04 -0500

    add av offset calculation and plot to scint. level page

--- minard/scintillator_level.py
+++ minard/scintillator_level.py
@@ -11,3 +11,17 @@ def get_scintillator_level(run_begin, run_end):
     rows = result.fetchall()
 
     return [dict(zip(keys,row)) for row in rows]
+
+
+def get_av_z_offset(run_begin, run_end):
+
+    conn = engine_nl.connect()
+
+    result = conn.execute("SELECT run::INTEGER, av_offset_z FROM av_offset WHERE " 
+                          "run >= %s AND run <= %s ORDER BY run", (run_begin, run_end))
+
+    keys = map(str, result.keys())
+    rows = result.fetchall()
+
+    return [dict(zip(keys,row)) for row in rows]
+
